7b28aa53810b8a529026e5ca9dd00d51fb3ceef1
My Git <451231778@qq.com>
2019-07-24 22:08:39 +0800

    这是第一次提交

---
+++ cmfz_ww/src/test/java/Test6.java
@@ -0,0 +1,15 @@
+import com.baizhi.PSVM;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PSVM.class)
+public class Test6 {
+    @Test
+    public void test1(){
+        System.out.println("sasasasas");
+        System.out.println("111111");
+    }
+}
